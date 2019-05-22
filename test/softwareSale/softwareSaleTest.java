c14f73d5db31534237eb73fac991991a57628b54
macstudent <macstudent@10.10.150.189>
2019-05-22 10:52:21 -0400

    r2 green

--- test/softwareSale/softwareSaleTest.java
+++ test/softwareSale/softwareSaleTest.java
@@ -12,5 +12,13 @@ public class softwareSaleTest {
 		double finalprice = s.calculatePrice(2);
 		assertEquals(198,finalprice,0);
 	}
+	@Test
+	public void testBuy10Package()
+	{
+		
+	softwareSale s = new softwareSale();
+	double finalPrice = s.calculatePrice(12);
+			assertEquals(950.4,finalPrice,0);
 
+}
 }
